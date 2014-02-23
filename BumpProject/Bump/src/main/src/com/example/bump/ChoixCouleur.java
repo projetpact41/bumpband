e8e7ca923575392646a66d7002d409dad8839ae1
projetpact41 <projetpact4.1@gmail.com>
2014-02-23 09:33:35 +0100

    Création classe ChoixCouleur

--- BumpProject/Bump/src/main/src/com/example/bump/ChoixCouleur.java
+++ BumpProject/Bump/src/main/src/com/example/bump/ChoixCouleur.java
@@ -1,5 +1,6 @@
 package com.example.bump;
 
+import android.R;
 import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Intent;
@@ -18,24 +19,21 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class MenuPrincipal extends Activity {
+public class ChoixCouleur extends Activity {
 
     private ListView listView;
-    private final String TAG = "MenuPrincipal";
+    private final String TAG = "ChoixCouleur";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.i(TAG,"Debut ListView");
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menuprincipal);
+        setContentView(R.layout.activity_choix_couleur);
 
         listView = (ListView) findViewById(R.id.list);
         Log.e(TAG,"Obtention listView");
-        //Definition des cases du menu principal
-        String[] cases = {"Liste de BumpFriend",
-                            "Choix couleur",
-                            "Simulation Bump",
-                            "A propos"};
+        //Definition des choix de couleurs possible
+        String[] cases = {"ROUGE", "BLEU", "VERT"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, android.R.id.text1, cases);
         listView.setAdapter(adapter);
@@ -51,22 +49,14 @@ public class MenuPrincipal extends Activity {
                 String  itemValue    = (String) listView.getItemAtPosition(position);
 
                 Log.e(TAG,itemValue);
-                if (itemValue.equals("Liste de BumpFriend")) {
-                    Intent i = new Intent(MenuPrincipal.this,BumpFriendListe.class);
-                    startActivity(i);
+                if (itemValue.equals("ROUGE")) {
+                    //bluetooth.allumerRouge();
                     Log.i(TAG,itemValue);
-                } else if (itemValue.equals("Choix couleur")) {
-                    //A faire en temps voulu
-                	Intent i = new Intent(MenuPrincipal.this,ChoixCouleur.class);
-                	startActivity(i);
+                } else if (itemValue.equals("BLEU")) {
+                    //bluetooth.allumerBleu();
                     Log.i(TAG,itemValue);
-                } else if (itemValue.equals("Simulation Bump")) {
-                    Intent i = new Intent (MenuPrincipal.this,SimuBump.class);
-                    startActivity(i);
-                    Log.i(TAG,itemValue);
-                } else if (itemValue.equals("A propos")) {
-                    Intent i = new Intent (MenuPrincipal.this,APropos.class);
-                    startActivity(i);
+                } else if (itemValue.equals("VERT")) {
+                   //bluetooth.allumerVert();
                     Log.i(TAG,itemValue);
                 }
 
@@ -115,3 +105,4 @@ public class MenuPrincipal extends Activity {
     }
 
 }
+
