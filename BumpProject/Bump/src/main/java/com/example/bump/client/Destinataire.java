e61cb5cd7ce71873b14cc14f2467645fe8ad64e9
Arturo <projetpact4.1@gmail.com>
2014-02-24 15:48:27 +0100

    Amélioration de l'admin(haslist identifiant etc)

--- BumpProject/Bump/src/main/java/com/example/bump/client/Destinataire.java
+++ BumpProject/Bump/src/main/java/com/example/bump/client/Destinataire.java
@@ -1,31 +1,19 @@
 package com.example.bump.client;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.util.Log;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
 
-import com.example.bump.actions.BumpFriend;
-import com.example.bump.actions.Color;
-import com.example.bump.actions.Connexion;
-import com.example.bump.actions.ErreurTransmission;
-import com.example.bump.actions.Message;
+import javax.naming.Context;
+
 import com.example.bump.actions.Transmissible;
 import com.example.bump.actions.Transmission;
 import com.example.bump.serveur.Parseur;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.InetAddress;
-import java.net.Socket;
-import java.net.UnknownHostException;
 
-/**
- * Created by jjuulliieenn on 01/01/14.
- */
+
 public class Destinataire {
 
     private InetAddress hostname;
@@ -39,25 +27,19 @@ public class Destinataire {
     public Destinataire (InetAddress hostname, int port) {
         this.hostname = hostname;
         this.port = port;
-        Log.i(TAG,"Creation du destinataire");
     }
 
-    public void envoieObjet (final Transmissible obj,final Context context) {
-        Log.i(TAG,"Envoi Objet");
+    public void envoieObjet (final Transmissible obj) {
         new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
                     socket = new Socket(hostname,port);
-                    Log.i(TAG,"Creation socket");
 
                     if (out == null) {
                     out = new DataOutputStream(socket.getOutputStream());
                     in = new DataInputStream(socket.getInputStream());
                     }
-
-
-                    Log.i(TAG,"Creation des flux");
 
                     b = obj.toBytes();
                     byte n = (byte) b.length;
@@ -70,37 +52,31 @@ public class Destinataire {
                     out.write(temp);
 
                     out.flush();
-                    Log.i(TAG,"Envoi des donnees");
 
                     if (!(obj instanceof Transmission)) {
                         Transmissible t = null;
                         Transmissible t2 = null;
 
                         try{
-                            Log.i(TAG,"Recuperation de l'objet");
 
                             byte size = in.readByte(); //Recoit la taille envoyee
-                            Log.i(TAG,"Taille recue " + size);
                             b = new byte[size];
                             in.read(b);
 
                             t = (Transmissible) Parseur.parser(b);
-                            t2 = t.execute(context);
-                            if (t2 != null) envoieObjet(t2,context);
+                            t2 = t.execute();
+                            if (t2 != null) envoieObjet(t2);
 
                        } catch (NullPointerException e) {
-                            Log.i(TAG, "Objet transmis invalide");
+                            System.out.println("Objet transmis invalide");
                         }
-                        //throw new TransmissionException (ErreurTransmission.MESSAGENONDELIVRE); @TODO gerer les exceptions
+                        //@TODO gerer les exceptions
                     }
-                    Log.i(TAG,"Reussite");
-                    //Intent intent = new Intent(context,com.example.bump.Reussie.class);
-                    //context.startActivity(intent);
 
                     Thread.sleep(5000);
 
                 } catch (IOException e) {
-                    Log.i(TAG,"Erreur");
+                    System.out.println("Erreur");
                     e.printStackTrace();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
